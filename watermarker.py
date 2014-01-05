6767696f54498d08b3a81699c74eca9b903668c7
Thomas Liu <thomasliu02@gmail.com>
2014-01-04 23:37:25 -0600

    Added a lot of gui stuff

--- watermarker.py
+++ watermarker.py
@@ -2,10 +2,129 @@
 
 import Image
 import os
-#class Spreader(QtGui.QMainWindow):
-#    def __init__(self):
-#        super(Spreader, self).__init__()
+
+class Watermarker(QtGui.QMainWindow):
+    def __init__(self):
+        super(Watermarker, self).__init__()
+        self.initUI()
         
+    def center(self):
+        '''Center the window'''
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+        
+    def initUI(self):
+        '''Initialize the UI'''
+        self.watermarker = Settings()
+        self.setCentralWidget(self.watermarker)
+        
+        #Actions
+        exitAction = QtGui.QAction(QtGui.QIcon('img/application-exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+        
+        #Statusbar
+        self.statusBar()
+        
+        #Menu bar
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        
+        fileMenu.addAction(exitAction)
+        
+        #Window Stuff
+        self.setWindowIcon(QtGui.QIcon('img/image.png'))
+#        self.resize(650,400)
+        self.center()
+        self.setWindowTitle('Watermarker')    
+        self.show()
+        
+class Settings(QtGui.QWidget):
+    def __init__(self):
+        super(Settings, self).__init__()
+        self.home = os.path.expanduser('~')
+        self.watermark = ""
+        self.inputFolder = ""
+        
+        
+        self.initUI()
+    
+    def initUI(self):
+        validator = QtGui.QIntValidator(0,9999999)
+        grid = QtGui.QGridLayout()
+        
+        #Watermark Edit
+        self.watermarkLabel = QtGui.QLabel('Watermark image')
+        
+        self.watermarkEdit = QtGui.QLineEdit()
+#        self.watermarkEdit.setValidator(validator)
+        self.watermarkEdit.setText(self.home)
+        
+        self.watermarkButton = QtGui.QPushButton('Browse')
+        
+        self.watermarkEdit.textChanged[str].connect(self.change)
+        self.watermarkButton.clicked.connect(self.get_watermark)
+        
+        #Input folder edit
+        self.inputFolderLabel = QtGui.QLabel('Input folder')
+        
+        self.inputFolderEdit = QtGui.QLineEdit()
+        self.inputFolderEdit.setText(self.home)
+        
+        self.inputFolderButton = QtGui.QPushButton('Browse')
+        
+        self.inputFolderEdit.textChanged[str].connect(self.change)
+        self.inputFolderButton.clicked.connect(self.get_folder)
+        
+        #Output folder edit
+        self.outputFolderLabel = QtGui.QLabel('Output folder')
+        
+        self.outputFolderEdit = QtGui.QLineEdit()
+        self.outputFolderEdit.setText(self.home)
+        
+        self.outputFolderButton = QtGui.QPushButton('Browse')
+        
+        self.outputFolderEdit.textChanged[str].connect(self.change)
+        self.outputFolderButton.clicked.connect(self.get_folder)
+        
+        #Get corner
+        self.cornerDict = {"Top Left":1 , "Top Right":2 , "Bottom Left":3 , "Bottom Right":4 }
+        self.combo = QtGui.Qself.comboBox(self)
+        for corner in self.cornerDict.keys():
+            self.combo.addItem(corner)
+        self.combo.activated[str].connect(self.change)
+        
+        #Get padding
+        self.paddingLabel = QtGui.QLabel('Padding (pixels or decimal\%)')
+        
+        self.paddingEdit = QtGui.QLineEdit()
+        self.paddingEdit.setValidator(validator)
+        self.paddingEdit.textChanged[str].connect(self.change)
+        
+        #Grid
+        grid.addWidget(self.watermarkLabel, 1, 0); grid.addWidget(self.watermarkEdit, 1, 1); grid.addWidget(self.watermarkButton, 1, 2)
+        grid.addWidget(self.inputFolderLabel, 2, 0); grid.addWidget(self.inputFolderEdit, 2, 1); grid.addWidget(self.inputFolderButton, 2, 2)
+        grid.addWidget(self.outputFolderLabel, 3, 0); grid.addWidget(self.outputFolderEdit, 3, 1); grid.addWidget(self.outputFolderButton, 3, 2)
+        
+        self.setLayout(grid)
+        
+    def get_watermark(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Select Watermark', self.home)
+        self.watermarkEdit.setText(fname)
+        
+    def get_folder(self):
+        sender = self.sender()
+        fname = QtGui.QFileDialog.getExistingDirectory(self,'Select Folder', self.home)
+        if sender is self.inputFolderButton:
+            self.inputFolderEdit.setText(fname)
+        elif sender is self.outputFolderButton:
+            self.outputFolderEdit.setText(fname)
+        
+    def change(self, s):
+        pass
         
 def find_pos(image, watermark, corner=4, padding=50):
     '''Return the coordinates the image should be pasted at'''
