f70d8cdccbb8a05c51f884eedd66c31eaee96c5b
Thomas Liu <thomasliu02@gmail.com>
2014-01-04 22:19:21 -0600

    Console input added

--- watermarker.py
+++ watermarker.py
@@ -2,6 +2,11 @@
 
 import Image
 import os
+#class Spreader(QtGui.QMainWindow):
+#    def __init__(self):
+#        super(Spreader, self).__init__()
+        
+        
 def find_pos(image, watermark, corner=4, padding=50):
     '''Return the coordinates the image should be pasted at'''
     width, height = image.size
@@ -33,9 +38,19 @@ def overlay_watermark(watermarkPath,inPath,outPath,corner=4,padding=50):
         except:
             print("Error with {0}".format(pic))
             raise
+def get_corner():
+    corners = [1,2,3,4]
+    while True:
+        try:
+            corner = int(raw_input("Which corner? \n(1 for top-left, 2 for top-right, 3 for bottom-left, and 4 for bottom-right):\n"))
+            if corner in corners:
+                return corner
+        except ValueError:
+            continue
         
 def main():
-    overlay_watermark("watermark.png","test","out")
+    print("Enter relative or absolute paths.")
+    overlay_watermark(raw_input("Watermark image path: "),raw_input("Input images folder: "),raw_input("Processed images folder: "))
             
 if __name__ == "__main__":
     main()
